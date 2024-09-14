0bc437e5e1e6fd1a5502aa6664da6802ebdca572
Vinay Patel <vinaypatel5519@gmai.com>
2024-09-14 11:50:58 +0530

    Enhance App.js with code splitting: - Implemented dynamic imports using React.lazy and Suspense for component code splitting. - Improved performance by loading components only when they are needed.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import NotFound from "./components/NotFound"; // Make sure to create this component
-import ForgotPassword from "./components/ForgotPassword";
-import Home from "./components/Home";
-import ProductDetails from "./components/ProductDetails";
-import Cart from "./components/Cart";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Import the CSS
-import Checkout from "./components/Checkout";
+import "react-toastify/dist/ReactToastify.css";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import UserProfile from "./components/UserProfile";
-import AboutUs from "./components/AboutUs";
-import Contact from "./components/ContactUs";
-import Privacy from "./components/Privacy";
-import Terms from "./components/Terms";
-import AdminProductForm from "./components/AdminProductForm";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const Home = lazy(() => import("./components/Home"));
+const Login = lazy(() => import("./components/Login"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const ProductDetails = lazy(() => import("./components/ProductDetails"));
+const Cart = lazy(() => import("./components/Cart"));
+const Checkout = lazy(() => import("./components/Checkout"));
+const UserProfile = lazy(() => import("./components/UserProfile"));
+const AboutUs = lazy(() => import("./components/AboutUs"));
+const Contact = lazy(() => import("./components/ContactUs"));
+const Privacy = lazy(() => import("./components/Privacy"));
+const Terms = lazy(() => import("./components/Terms"));
+const AdminProductForm = lazy(() => import("./components/AdminProductForm"));
+const ForgotPassword = lazy(() => import("./components/ForgotPassword"));
+const NotFound = lazy(() => import("./components/NotFound"));
 
 function App() {
   return (
     <>
-      <ToastContainer />{" "}
-      {/* This is where toast notifications will be rendered */}
+      <ToastContainer />
       <Router>
         <Elements stripe={stripePromise}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/user-profile" element={<UserProfile />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/admin" element={<AdminProductForm />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/product/:id" element={<ProductDetails />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/user-profile" element={<UserProfile />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/admin" element={<AdminProductForm />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </Elements>
       </Router>
     </>
